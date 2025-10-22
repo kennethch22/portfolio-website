5461d725e53000d2c152a036a43053b95ea2fd4a
Kenneth <kennethchristopherhsg@gmail.com>
2025-10-23 06:06:35 +0800

    temporarily removed cursor

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
           <Navbar />
           <div className="pt-20">{children}</div>
           <Footer />
-            <MagicCursor />
         </SessionProvider>
       </body>
     </html>
